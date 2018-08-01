857cd283abb6b993ae718168c8c74793330c237a
Yusuf Salman Noerinaldy Siregar <33023147+yusufsiregar44@users.noreply.github.com>
2018-08-02 00:49:41 +0700

    Delete firebaseConfig.js

--- client/src/helpers/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyAsJy4Yfe-dRNkRKFGuUTZVDTVZZ1-YG5k",
-  authDomain: "hcktivovrflw.firebaseapp.com",
-  databaseURL: "https://hcktivovrflw.firebaseio.com",
-  projectId: "hcktivovrflw",
-  storageBucket: "hcktivovrflw.appspot.com",
-  messagingSenderId: "686412817446"
-};
